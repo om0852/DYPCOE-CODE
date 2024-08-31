3aa1056c5bb9fa960bd00df12c3709f2d952dfe5
om salunke <omsaunke343@gmail.com>
2024-09-01 00:44:17 +0530

    pr1 completed

---
+++ oop/pr_1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+class Complex{
+		public:
+	int real;
+	int img;
+
+	Complex(){
+		real=0;
+		img=0;
+	}
+	
+    Complex operator +( Complex& c)  {
+        Complex temp;
+        temp.real = real + c.real;
+        temp.img = img + c.img;
+        return temp;
+    }
+
+
+ friend istream& operator>>(istream& input, Complex& c) {
+        cout << "Enter Real No: ";
+        input >> c.real;
+        cout << "Enter Img No: ";
+        input >> c.img;
+        return input;
+    }
+
+    friend ostream& operator<<(ostream& output, const Complex& c) {
+        output << c.real << "+" << c.img << "i";
+        return output;
+    }
+};
+int main(){
+ Complex c1, c2;
+
+    cin >> c1 >> c2;
+    Complex c3 = c1 + c2;
+
+    cout << "Sum: " << c3 << endl;
+
+	return 0;
+}
