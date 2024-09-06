087b05db80faf57276e839577bad2980195eab26
om salunke <omsaunke343@gmail.com>
2024-09-06 23:50:26 +0530

    cohen part 1 completed

--- cg/cohensutherland.cpp
+++ cg/cohensutherland.cpp
@@ -1,92 +1,60 @@
-#include <iostream>
-#include <graphics.h>
+#include<iostream>
+#include<graphics.h>
 
-using namespace std;
-
-// Define the clipping window boundaries
-const int xmin = 100, xmax = 400, ymin = 100, ymax = 400;
-
-// Define the outcode constants
-const int LEFT = 1, RIGHT = 2, BOTTOM = 4, TOP = 8, INSIDE = 0;
-
-// Function to compute the outcode for a point
-int computeOutcode(int x, int y) {
-    int code = INSIDE;
-    if (x < xmin) code |= LEFT;
-    if (x > xmax) code |= RIGHT;
-    if (y < ymin) code |= BOTTOM;
-    if (y > ymax) code |= TOP;
-    return code;
-}
-
-// Cohen-Sutherland line clipping algorithm
-void cohenSutherlandClip(int &x1, int &y1, int &x2, int &y2) {
-    int outcode1 = computeOutcode(x1, y1);
-    int outcode2 = computeOutcode(x2, y2);
-    bool accept = false;
-
-    while (true) {
-        if (!(outcode1 | outcode2)) {
-            // Both points inside
-            accept = true;
-            break;
-        } else if (outcode1 & outcode2) {
-            // Both points outside
-            break;
-        } else {
-            int outcodeOut = outcode1 ? outcode1 : outcode2;
-            int x, y;
-
-            // Find intersection point
-            if (outcodeOut & TOP) {
-                x = x1 + (x2 - x1) * (ymax - y1) / (y2 - y1);
-                y = ymax;
-            } else if (outcodeOut & BOTTOM) {
-                x = x1 + (x2 - x1) * (ymin - y1) / (y2 - y1);
-                y = ymin;
-            } else if (outcodeOut & RIGHT) {
-                y = y1 + (y2 - y1) * (xmax - x1) / (x2 - x1);
-                x = xmax;
-            } else if (outcodeOut & LEFT) {
-                y = y1 + (y2 - y1) * (xmin - x1) / (x2 - x1);
-                x = xmin;
-            }
-
-            // Replace outside point with intersection point
-            if (outcodeOut == outcode1) {
-                x1 = x;
-                y1 = y;
-                outcode1 = computeOutcode(x1, y1);
-            } else {
-                x2 = x;
-                y2 = y;
-                outcode2 = computeOutcode(x2, y2);
-            }
-        }
+// Function to calculate the intersection point
+void calculateIntersection(int x1, int y1, int x2, int y2, int xmin, int ymin, int xmax, int ymax, int code, int &x, int &y) {
+    if (code == 1) {  // Line is to the left of the rectangle
+        y = y1 + (y2 - y1) * (xmin - x1) / (x2 - x1);
+        x = xmin;
     }
-
-    if (accept) {
-        // Draw the line if it is accepted
-        line(x1, y1, x2, y2);
+    else if (code == 2) {  // Line is to the right of the rectangle
+        y = y1 + (y2 - y1) * (xmax - x1) / (x2 - x1);
+        x = xmax;
     }
+    else if (code == 4) {  // Line is above the rectangle
+        x = x1 + (x2 - x1) * (ymin - y1) / (y2 - y1);
+        y = ymin;
+    }
+    else if (code == 8) {  // Line is below the rectangle
+        x = x1 + (x2 - x1) * (ymax - y1) / (y2 - y1);
+        y = ymax;
+    }
+    return x,y;
 }
 
 int main() {
+    int code1 = 0, code2 = 0;
     int gd = DETECT, gm;
     initgraph(&gd, &gm, NULL);
 
-    // Draw the clipping window
+    int xmin = 100, ymin = 100, xmax = 400, ymax = 400;
     rectangle(xmin, ymin, xmax, ymax);
 
-    // Define the line endpoints
-    int x1 = 200, y1 = 200, x2 = 500, y2 = 200;
+    int x1 = 120, y1 = 120, x2 = 500, y2 = 200;
+    line(x1, y1, x2, y2);  // Draw initial line
 
-    // Clip the line using Cohen-Sutherland algorithm
-    cohenSutherlandClip(x1, y1, x2, y2);
+    // Determine region codes for p1 and p2
+    if (x2 < xmin) code2 |= 1;  // Left
+    if (x2 > xmax) code2 |= 2;  // Right
+    if (y2 < ymin) code2 |= 4;  // Above
+    if (y2 > ymax) code2 |= 8;  // Below
 
-    getch();
+    int x, y;
+
+    // Clear the screen before drawing the second line
+    delay(1000);
+    cleardevice();
+    rectangle(xmin, ymin, xmax, ymax);  // Redraw the rectangle
+
+    // Calculate the new intersection point for p2
+    if (code2 != 0) {
+        calculateIntersection(x1, y1, x2, y2, xmin, ymin, xmax, ymax, code2, x, y);
+        line(x1, y1, x, y);  // Draw the line after clipping
+    } else {
+        line(x1, y1, x2, y2);  // If p2 is inside, draw the original line
+    }
+
     delay(10000);
     closegraph();
-
     return 0;
 }
