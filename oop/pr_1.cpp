27a641d7b46975bf3294f42cbe93669e649212de
om salunke <omsaunke343@gmail.com>
2024-10-20 17:37:29 +0530

    vector program done

--- oop/pr_1.cpp
+++ oop/pr_1.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
 using namespace std;
 
-class Complex{
-		public:
-	int real;
-	int img;
+class Complex
+{
+public:
+    int real;
+    int img;
 
-	Complex(){
-		real=0;
-		img=0;
-	}
-	
-    Complex operator +( Complex& c)  {
+    Complex()
+    {
+        real = 0;
+        img = 0;
+    }
+
+    Complex operator+(Complex &c)
+    {
         Complex temp;
         temp.real = real + c.real;
         temp.img = img + c.img;
         return temp;
     }
 
-
- friend istream& operator>>(istream& input, Complex& c) {
+    friend istream &operator>>(istream &input, Complex &c)
+    {
         cout << "Enter Real No: ";
         input >> c.real;
         cout << "Enter Img No: ";
@@ -27,18 +30,20 @@ class Complex{
         return input;
     }
 
-    friend ostream& operator<<(ostream& output, const Complex& c) {
+    friend ostream &operator<<(ostream &output, const Complex &c)
+    {
         output << c.real << "+" << c.img << "i";
         return output;
     }
 };
-int main(){
- Complex c1, c2;
+int main()
+{
+    Complex c1, c2;
 
     cin >> c1 >> c2;
     Complex c3 = c1 + c2;
 
     cout << "Sum: " << c3 << endl;
 
-	return 0;
+    return 0;
 }
